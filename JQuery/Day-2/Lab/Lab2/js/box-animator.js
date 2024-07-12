9954e0a893c6a26e916e3d304d6b031269744a1b
ahmednabil14 <ahmednabil14499@gmail.com>
2024-07-12 18:50:06 +0300

    JQ day2

---
+++ JQuery/Day-2/Lab/Lab2/js/box-animator.js
@@ -0,0 +1,23 @@
+$('div').animate({
+    left: '+=500px',
+    width: '200px',
+    height: '200px',
+}, 1000)
+
+$('div').animate({
+    top: '+=500px',
+    width: '100px',
+    height: '100px',
+}, 1000)
+
+$('div').animate({
+    left: '-=500px',
+    width: '200px',
+    height: '200px',
+}, 1000)
+
+$('div').animate({
+    top: '-=500px',
+    width: '100px',
+    height: '100px',
+}, 1000)
