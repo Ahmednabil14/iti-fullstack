5d340499febba8fa9e162f50fdb1ee0c690a02a4
ahmednabil14 <ahmednabil14499@gmail.com>
2024-08-31 19:17:47 +0300

    lab 1

--- Flask/Day-1/Lab/blog.py
+++ Flask/Day-1/Lab/blog.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
 from flask_sqlalchemy import SQLAlchemy
 
 blog = Flask(__name__)
@@ -15,6 +15,43 @@ class Post(db.Model):
     image = db.Column(db.String, nullable=True)
 
 
-@blog.route("/")
+@blog.route("/", endpoint="home")
 def home():
-    return render_template("post/home.html")
+    posts = Post.query.all()
+    return render_template("post/home.html", posts=posts)
+
+@blog.route("/Details/<int:id>", endpoint="post_details")
+def details(id):
+    post = Post.query.get(id)
+    return render_template("post/details.html", post=post)
+
+@blog.route("/Delete/<int:id>", endpoint="delete")
+def delete(id):
+    post = Post.query.get(id)
+    db.session.delete(post)
+    db.session.commit()
+    return redirect(url_for("home"))
+
+@blog.route("/Create", endpoint="create", methods=["GET", "POST"])
+def create():
+    if request.method == "POST":
+        print(request.form)
+        post = Post(name=request.form["name"],
+                    description=request.form["description"],
+                    image=request.form["image"])
+        db.session.add(post)
+        db.session.commit()
+        return redirect(url_for("home"))
+    return render_template("post/create.html")
+
+@blog.route("/Edit/<int:id>", endpoint="update", methods=["GET", "POST"])
+def update(id):
+    post = Post.query.get(id)
+    if request.method == "POST":
+        for index, field in enumerate(request.form):
+            if request.form[field]:
+                setattr(post, field, request.form[field])
+                db.session.commit()
+        return redirect(url_for("home"))
+    return render_template("post/update.html", post=post)
+
